50501a0f39bbef0dc7ecffbb1a9f56baa986d2fc
elaynne torres <elaynne.ufs@gmail.com>
2019-04-08 23:54:49 -0300

    Initial commit - HeapSort

---
+++ DataStructureAPI/Models/InputElements.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DataStructureAPI.Models
+{
+    public class InputElements
+    {
+        public int[] Itens { get; set; } 
+    }
+}
